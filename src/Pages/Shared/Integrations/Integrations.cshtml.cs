107b0ef4a508307b4ce6c8c06034547b70f7cca9
Sandeep <43872892+qdevelopergithub@users.noreply.github.com>
2024-06-24 10:37:23 +0530

    Integration-page-layout

---
+++ src/Pages/Shared/Integrations/Integrations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Integrations
+{
+    public class IntegrationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
