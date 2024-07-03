29e21dd7caeedfe7e4b19b30ce18fecc97f52215
Sandeep <qdeveloper.github@gmail.com>
2024-07-03 14:24:56 +0530

    header desktop menus and header logos

---
+++ src/Pages/Shared/Logos/ProductMenu/DataManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Logos.ProductMenu
+{
+    public class DataManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Pages/Shared/Logos/ProductMenu/DesktopMenuMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Logos.ProductMenu
+{
+    public class DesktopMenuMapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
