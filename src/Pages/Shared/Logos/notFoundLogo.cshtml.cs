52cd909caf7cd7ccd583a5d04a0c9243535776eb
Sandeep <qdeveloper.github@gmail.com>
2024-07-03 18:29:30 +0530

    not found page init

---
+++ src/Pages/Shared/Logos/notFoundLogo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Logos
+{
+    public class notFoundLogoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
