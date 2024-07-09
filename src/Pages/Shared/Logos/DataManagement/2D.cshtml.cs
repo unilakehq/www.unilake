6657b2b2d2ffb820682333dcb8ef98641e28e252
Sandeep <qdeveloper.github@gmail.com>
2024-07-09 12:16:07 +0530

    Data Management page Design for Desktop  Init

---
+++ src/Pages/Shared/Logos/DataManagement/2D.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Logos.DataManagement
+{
+    public class _2DModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Pages/Shared/Logos/DataManagement/Illustration_Easily_Find.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.unilake.Pages.Shared.Logos.DataManagement
+{
+    public class Illustration_Easily_FindModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
